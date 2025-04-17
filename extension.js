f0af09f7ff801db0e5b0e1b724c5b224c0e81f6f
Gerardo Zúñiga <TheRambien@hotmail.com>
2025-04-17 14:12:54 -0500

    add new logic for inyected dynamic css

--- extension.js
+++ extension.js
@@ -1,32 +1,73 @@
 import St from 'gi://St';
+import Gio from 'gi://Gio';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 
-export default class blurquicksettingsExtension extends Extension {
+export default class transparencyExtension extends Extension {
+    
+    constructor(metadata) {
+        super(metadata);
+        this._settings     = null;
+        this._styleTag     = null;
+        this._settingsId   = null;
+    }
     enable() {
-        this._addCustomStyles();
+        this._settings = this.getSettings('org.gnome.shell.extensions.transparency-panel');
+        this._settingsId = this._settings.connect('changed', () => this._applyStyles());
+        this._applyStyles();
     }
 
     disable() {
+        if (this._settingsId) {
+            this._settings.disconnect(this._settingsId);
+            this._settingsId = null;
         this._removeCustomStyles();
+        this._settings = null;
+        }
     }
 
-    _addCustomStyles() {
-        // Agrega la clase personalizada a los elementos deseados
-        Main.panel.add_style_class_name('translucent-panel');
+    _applyStyles() {
+        const useCustomStyle = this._settings.get_boolean('use-custom-style');
 
-        const quickSettings = Main.panel.statusArea.quickSettings;
-        if (quickSettings) {
-            quickSettings.menu.actor.add_style_class_name('translucent-qs');
+        if(useCustomStyle) {
+            const opacity = this._settings.get_int('panel-opacity') / 100;
+
+            const css = `
+                #panel {
+                    background-color: rgba(50, 50, 50, ${opacity}; 
+                }
+            
+            `;
+            if (this._styleTag) {
+                this._styleTag.set_content(css);
+            } else {
+                this._styleTag = St.ThemeContext.get_for_stage(global.stage)
+                    .get_theme()
+                    .append_stylesheet_from_string(css);
+            }
+        } else {
+            Main.panel.add_style_class_name('CSSstylePanel');
+            const addStyleClass = Main.panel.statusArea.quickSettings;
+                if (addStyleClass) {
+                    addStyleClass.menu.actor.add_style_class_name('CSSstylePanel-qs');
+                }
         }
     }
     _removeCustomStyles() {
-        Main.panel.remove_style_class_name('translucent-panel');
+        const useCustomStyle = this._settings.get_boolean('use-custom-style');
 
-        const quickSettings = Main.panel.statusArea.quickSettings;
-        if (quickSettings) {
-            quickSettings.menu.actor.remove_style_class_name('translucent-qs');
+        if (useCustomStyle && this._styleTag){
+                St.ThemeContext.get_for_stage(global.stage)
+                    .get_theme()
+                    .remove_stylesheet(styleTag);
+                styleTag = null;
+        } else {
+            Main.panel.remove_style_class_name('CSSstylePanel');
+            const addStyleClass = Main.panel.statusArea.quickSettings;
+            if (addStyleClass) {
+                addStyleClass.menu.actor.remove_style_class_name('CSSstylePanel-qs');
+            }
         }
     }
+    
 }
-
