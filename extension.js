ccb7a39602bae7e14eb5f9d63782685cf5989eec
Gerardo Zúñiga <TheRambien@hotmail.com>
2025-04-17 03:23:01 -0500

    extension only apply styles css

--- extension.js
+++ extension.js
@@ -2,10 +2,6 @@ import St from 'gi://St';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 
-import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
-import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
-import * as QuickSettings from 'resource:///org/gnome/shell/ui/quickSettings.js';
-
 export default class blurquicksettingsExtension extends Extension {
     enable() {
         this._addCustomStyles();
