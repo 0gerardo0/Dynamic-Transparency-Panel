95cc3ec614a7c781359427092cb0a7fecbbbc8f1
Gerardo Zúñiga <TheRambien@hotmail.com>
2025-04-18 22:45:30 -0500

    include a funtional prefs styles - reorganitation css file

--- extension.js
+++ extension.js
@@ -1,31 +1,135 @@
-import St from 'gi://St';
-import Gio from 'gi://Gio';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+import Gio from 'gi://Gio';
+import GLib from 'gi://GLib';
+import St from 'gi://St';
 
-export default class transparencyExtension extends Extension {
+export default class TransparencyExtension extends Extension {
+    constructor(metadata) {
+        super(metadata);
+        this._settings = null;
+        this._settingsChangedId = null;
+        this._styleChangedTimeoutId = null;
+        this._overviewShowingId = null;
+        this._overviewHidingId = null;
+        this._panelStyleOriginal = null;
+    }
 
     enable() {
+        this._settings = this.getSettings('org.gnome.shell.extensions.transparency-panel');
+        
+        // Conectar la señal de cambio de configuraciones
+        this._settingsChangedId = this._settings.connect('changed', this._onSettingsChanged.bind(this));
+        
+        // Conectar señales para detectar cambios en el Overview
+        this._overviewShowingId = Main.overview.connect('showing', this._onOverviewShowing.bind(this));
+        this._overviewHidingId = Main.overview.connect('hiding', this._onOverviewHiding.bind(this));
+        
+        // Aplicar estilos iniciales
         this._applyStyles();
     }
 
     disable() {
+        // Desconectar la señal de cambio de configuraciones
+        if (this._settingsChangedId) {
+            this._settings.disconnect(this._settingsChangedId);
+            this._settingsChangedId = null;
+        }
+        
+        // Desconectar señales del Overview
+        if (this._overviewShowingId) {
+            Main.overview.disconnect(this._overviewShowingId);
+            this._overviewShowingId = null;
+        }
+        
+        if (this._overviewHidingId) {
+            Main.overview.disconnect(this._overviewHidingId);
+            this._overviewHidingId = null;
+        }
+        
+        // Eliminar cualquier timeout pendiente
+        if (this._styleChangedTimeoutId) {
+            GLib.source_remove(this._styleChangedTimeoutId);
+            this._styleChangedTimeoutId = null;
+        }
+        
+        // Eliminar estilos personalizados
         this._removeCustomStyles();
+        
+        this._settings = null;
+    }
+
+    _onSettingsChanged(settings, key) {
+        // Evitar múltiples actualizaciones rápidas estableciendo un pequeño timeout
+        if (this._styleChangedTimeoutId) {
+            GLib.source_remove(this._styleChangedTimeoutId);
+        }
+        
+        this._styleChangedTimeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 300, () => {
+            this._applyStyles();
+            this._styleChangedTimeoutId = null;
+            return GLib.SOURCE_REMOVE;
+        });
+    }
+
+    _onOverviewShowing() {
+        // Guardar el estilo actual para restaurarlo después
+        this._panelStyleOriginal = Main.panel.style;
+    }
+
+    _onOverviewHiding() {
+        // Reaplica los estilos cuando se sale del Overview
+        GLib.timeout_add(GLib.PRIORITY_DEFAULT, 100, () => {
+            this._applyStyles();
+            return GLib.SOURCE_REMOVE;
+        });
     }
 
     _applyStyles() {
+        // Aplicar clase CSS para estilos estáticos
         Main.panel.add_style_class_name('CSSstylePanel');
-            const addStyleClass = Main.panel.statusArea.quickSettings;
-            if (addStyleClass) {
-                addStyleClass.menu.actor.add_style_class_name('CSSstylePanel-qs');
-            }
+        
+        const addStyleClass = Main.panel.statusArea.quickSettings;
+        if (addStyleClass) {
+            addStyleClass.menu.actor.add_style_class_name('CSSstylePanel-qs');
         }
+        
+        // No aplicar opacidad personalizada si estamos en el Overview
+        if (Main.overview.visible) {
+            return;
+        }
+        
+        // Obtener valor de opacidad del panel de configuración (0-100)
+        const panelOpacity = this._settings.get_int('panel-opacity');
+        
+        // Convertir a formato CSS (0-1)
+        const opacityValue = panelOpacity / 100;
+        
+        // Crear un estilo CSS dinámico para el panel
+        const panelStyle = `background-color: rgba(50, 50, 50, ${opacityValue});`;
+        
+        // Aplicar el estilo directamente
+        Main.panel.style = panelStyle;
+        
+        // Configurar un timeout para asegurar que el estilo se mantiene
+        // Esto ayuda a evitar que GNOME Shell sobreescriba nuestro estilo
+        GLib.timeout_add(GLib.PRIORITY_DEFAULT, 200, () => {
+            if (!Main.overview.visible && Main.panel.style !== panelStyle) {
+                Main.panel.style = panelStyle;
+            }
+            return GLib.SOURCE_REMOVE;
+        });
+    }
 
     _removeCustomStyles() {
         Main.panel.remove_style_class_name('CSSstylePanel');
+        
         const addStyleClass = Main.panel.statusArea.quickSettings;
         if (addStyleClass) {
             addStyleClass.menu.actor.remove_style_class_name('CSSstylePanel-qs');
         }
+        
+        // Eliminar estilos dinámicos
+        Main.panel.style = null;
     }
 }
--- prefs.js
+++ prefs.js
@@ -9,37 +9,62 @@ export default class PanelSettingsPrefs extends ExtensionPreferences {
         const settings = this.getSettings('org.gnome.shell.extensions.transparency-panel');
 
         const page = new Adw.PreferencesPage({
-            title: _('Appearence'),
+            title: _('Appearance'),
         });
         
         const group = new Adw.PreferencesGroup({
-          title: _('Top Bar Style'),
+            title: _('Top Bar Style'),
+            description: _('Configure the appearance of the top bar')
         });
 
+        // Panel Opacity slider
         const panelOpacity = new Gtk.Scale({
-          orientation: Gtk.Orientation.HORIZONTAL,
-          adjustment: new Gtk.Adjustment({
-            lower: 0,
-            upper: 100,
-            step_increment:1,
-          }),
-          digits: 0,
-          draw_value: true,
+            orientation: Gtk.Orientation.HORIZONTAL,
+            adjustment: new Gtk.Adjustment({
+                lower: 0,
+                upper: 100,
+                step_increment: 1,
+            }),
+            digits: 0,
+            draw_value: true,
+            value_pos: Gtk.PositionType.RIGHT,
+            hexpand: true,
         });
+        
         panelOpacity.set_value(settings.get_int('panel-opacity'));
         panelOpacity.connect('value-changed', () => {
-          settings.set_int('panel-opacity', panelOpacity.get_value());
+            settings.set_int('panel-opacity', panelOpacity.get_value());
         });
-        group.add(buildRow(_('Panel Opaciity'), panelOpacity));
+        
+        group.add(buildRow(_('Panel Opacity'), panelOpacity));
+
+        // Añadir más configuraciones aquí en el futuro
+        // Por ejemplo, para un selector de color:
+        /*
+        const colorButton = new Gtk.ColorButton();
+        
+        // Convertir string de color a objeto GdkRGBA
+        const colorString = settings.get_string('panel-color');
+        const rgba = new Gdk.RGBA();
+        rgba.parse(colorString);
+        colorButton.set_rgba(rgba);
+        
+        colorButton.connect('color-set', () => {
+            const color = colorButton.get_rgba().to_string();
+            settings.set_string('panel-color', color);
+        });
+        
+        group.add(buildRow(_('Panel Color'), colorButton));
+        */
 
         page.add(group);
         window.add(page);
     }
 }
-function buildRow(title, widget) {
-  const row = new Adw.ActionRow({ title });
-  row.add_suffix(widget);
-  widget.set_hexpand(true);
-  return row;
-}
 
+function buildRow(title, widget) {
+    const row = new Adw.ActionRow({ title });
+    row.add_suffix(widget);
+    widget.set_hexpand(true);
+    return row;
+}
